680bf7b11abd51b872ee90ecd9e1c00d78dc4190
edo9k <edox86@gmail.com>
2019-03-20 19:07:18 -0300

    Added MongoDB to project
    It was added in a very unsafe way, which is not a problem in this
    particular case because the mlab database is disposable, but is
    nontheless horrible pratice.

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+
+// don't do this
+// this shit ain't safe at all
+
+module.exports = {
+  mongoURI: 'mongodb://dbuser:dbuserpass123@ds335275.mlab.com:35275/mern-course'
+}
+
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+// db config
+const db = require('./config/keys').mongoURI
+
+// connect to mongodb
+mongoose
+  .connect(db)
+  .then(() => console.log('mongodb connected'))
+  .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('hello'))
 
