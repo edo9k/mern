66568eb11432ebe36b52853a53039b59fae2335f
edo9k <edox86@gmail.com>
2019-03-21 18:09:57 -0300

    Added mongoose, routes and models
    All changes up to classe 9. Also define the User scheema with mongoose.

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+const users = require('./routes/api/users')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
+
 const app = express()
 
 // db config
@@ -14,6 +18,12 @@ mongoose
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('hello'))
+
+
+// use routes
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
 
 const port = process.env.PORT || 5000
 
