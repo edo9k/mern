2466902215e94640987e6573ec8d4a811060f132
edo9k <edox86@gmail.com>
2019-04-09 08:51:13 -0300

    Up to Class 37
    Forgot to commit yesterday, in this part of the course we're working on
    the beginning of the frontend, just translating things from pure html to
    JSX and doing some basic interactions.
    There are a bunch of bugs though, and the guy who made the course
    doesn't really pay much attention to the problems, so we're left on our
    own to figure them out (which is good practice, but bad form when you've
    paid for the course and expect the minimum support).

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,12 +1,32 @@
 /** @format */
 
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Landing extends Component {
   render() {
     return (
-      <div>
-        DingDongs <strong>blings</strong> <sub>blongs</sub>
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">Developer Connector</h1>
+                <p className="lead">
+                  {' '}
+                  Create a Developer profile/portifolio, share posts and get help from other developers.
+                </p>
+                <hr />
+                <Link to="/register" className="btn btn-lg btn-info mr-2">
+                  Sign Up
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-light">
+                  Login
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,10 +1,37 @@
 /** @format */
 
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Navbar extends Component {
   render() {
-    return <div>DivSoup!</div>
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            DevConnector
+          </Link>
+
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav" />
+          <span className="navbar-toggler-icon" />
+        </div>
+
+        <div className="collapse navbar-collapse" id="mobile-nav">
+          <Link className="nav-item" to="/profiles">
+            Developers
+          </Link>
+
+          <ul className="navbar-nav ml-auto">
+            <Link className="nav-link" to="/register">
+              Sign Up
+            </Link>
+            <Link className="nav-link" to="/login">
+              Login
+            </Link>
+          </ul>
+        </div>
+      </nav>
+    )
   }
 }
 
